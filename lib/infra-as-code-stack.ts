b6bac3aa5973435209501ea090723320565c3be3
Felipe Matos <felipematoswb@gmail.com>
2021-11-30 12:14:07 -0300

    changes

--- lib/infra-as-code-stack.ts
+++ lib/infra-as-code-stack.ts
@@ -1,15 +1,61 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class InfraAsCodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 1,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+        {
+          name: 'ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraAsCodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dbInstance = new rds.DatabaseInstance(this, 'db-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_13_1,
+      }),
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'),
+      multiAz: false,
+      allocatedStorage: 10,
+      maxAllocatedStorage: 20,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(0),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      databaseName: 'todosdb',
+      publiclyAccessible: false,
+    });
+
+    new cdk.CfnOutput(this, 'dbEndpoint', {
+      value: dbInstance.instanceEndpoint.hostname,
+    });
+
+    new cdk.CfnOutput(this, 'secretName', {
+      value: dbInstance.secret?.secretName!,
+    });
   }
 }
