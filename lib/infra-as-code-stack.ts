8348451ca4e65606f2cb4c0b1a37ca75a8e5d887
Felipe Matos <felipematoswb@gmail.com>
2021-11-30 14:33:53 -0300

    Add ec2 and allow access RDS

--- lib/infra-as-code-stack.ts
+++ lib/infra-as-code-stack.ts
@@ -1,29 +1,89 @@
 import * as cdk from '@aws-cdk/core';
 import * as rds from '@aws-cdk/aws-rds';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class InfraAsCodeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create a custom VPC and subnets
     const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
       cidr: '10.0.0.0/16',
       natGateways: 1,
       maxAzs: 3,
       subnetConfiguration: [
         {
-          name: 'application',
+          name: 'database',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+        {
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
           cidrMask: 24,
         },
         {
           name: 'ingress',
           subnetType: ec2.SubnetType.PUBLIC,
-          cidrMask: 24,
-        },
+          cidrMask: 24
+        }
       ],
     });
 
+    // Ever create you owner role/policy, dont not use managed AWS Role.
+    const role = new iam.Role(this, 'MySSMtoAccessEC2', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: [
+        "ssm:DescribeAssociation",
+        "ssm:GetDeployablePatchSnapshotForInstance",
+        "ssm:GetDocument",
+        "ssm:DescribeDocument",
+        "ssm:GetManifest",
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:ListAssociations",
+        "ssm:ListInstanceAssociations",
+        "ssm:PutInventory",
+        "ssm:PutComplianceItems",
+        "ssm:PutConfigurePackageResult",
+        "ssm:UpdateAssociationStatus",
+        "ssm:UpdateInstanceAssociationStatus",
+        "ssm:UpdateInstanceInformation",
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel",
+        "ec2messages:AcknowledgeMessage",
+        "ec2messages:DeleteMessage",
+        "ec2messages:FailMessage",
+        "ec2messages:GetEndpoint",
+        "ec2messages:GetMessages",
+        "ec2messages:SendReply"
+      ],
+    }));
+
+    // Create a instance
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+      },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      role: role
+    });
+
+    // Create a RDS instance
     const dbInstance = new rds.DatabaseInstance(this, 'db-instance', {
       vpc,
       vpcSubnets: {
@@ -50,10 +110,15 @@ export class InfraAsCodeStack extends cdk.Stack {
       publiclyAccessible: false,
     });
 
+    // Allow connections from EC2 SG
+    dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432));
+
+    // Print instance endpoint
     new cdk.CfnOutput(this, 'dbEndpoint', {
       value: dbInstance.instanceEndpoint.hostname,
     });
 
+    // Print Secret name
     new cdk.CfnOutput(this, 'secretName', {
       value: dbInstance.secret?.secretName!,
     });
